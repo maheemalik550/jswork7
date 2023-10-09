5a3a008da28728809061dd2a417766f493175926
maheemalik550 <maheemalik550@gmail.com>
2023-10-09 14:49:57 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// function get(){
+//    alert("hellow world");
+// }
+// function greet(a){
+//     console.log(a);
+//     a.style.backgroundColor="red"
+//     a.style.color="blue"
+//     a.style.border="white"
+// }
+
+// function get(){
+//     var a = document.getElementById("userinput1");
+//     var b = document.getElementById("userinput2");
+//     var c = document.getElementById("userinput3");
+    
+//     if(a.value && b.value && c.value){
+//         console.log(a.value,b.value,c.value);
+//     }else{
+//         alert("please fill the all");
+//     }
+// }
+// <!-- CHAPTER 46-48 -->
+
+
+// function get(){
+//     var audio = new Audio('interface-124464.mp3')
+//     audio.play();
+// }
+// function get(){
+    //     var a = document.getElementById("userinput");
+    //     console.log(a.value);
+    //     userinput.value=" ";
+    // // }
+    // function get(){
+    //     var a = document.getElementById("value1");
+    //     console.log(a.value)
+    // }
+    
+    // function get(){
+    //     var a = document.getElementById("userinput");
+    //     console.log(a.value);
+    // }
+    // function get(){
+    //     var a = document.getElementById("enter");
+    //     console.log(a.value); 
+    // }
+
+// Q 7 form 
+
+function greet(){
+    var a = document.getElementById("value1")
+    var a = document.getElementById("value2")
+    var a = document.getElementById("value3")
+    var a = document.getElementById("value4")
+    var a = document.getElementById("value5")
+    var a = document.getElementById("value6")
+    // var a = document.getElementById("value7")
+    console.log(value1.value,value2.value,value3.value,value4.value,value5.value,value6.value);
+
+
+
+}
+
+
+
